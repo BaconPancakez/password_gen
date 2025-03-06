cd11b8dce7fce8dcb4943f0740041808e7796183
Qi Ming <qmseng15@gmail.com>
2025-03-07 03:08:33 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let box1 = document.getElementById("box1-h5")
+let box2 = document.getElementById("box2-h5")
+
+let passwordLength = 12
+
+function getRandomCharacter() {
+    let randomChar = Math.floor(Math.random() * characters.length)
+    return characters[randomChar]
+}
+
+function generateRandomPassword() {
+    let randomPassword = ""
+    for (let i = 0; i < passwordLength; i++) {
+        randomPassword += getRandomCharacter()         
+    }
+    return randomPassword
+}
+
+function display(){
+    box1.textContent = generateRandomPassword()
+    box2.textContent = generateRandomPassword()
+}
